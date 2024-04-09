22aaa165b7af3275e6ba373a97a96703cc730657
$BYD <bydquest@gmail.com>
2024-04-09 12:35:44 +0800

    update

--- main.go
+++ main.go
@@ -2,11 +2,8 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"time"
 	"github.com/BYDquest/RRRDoodlers/utility")
 
-func main() {
-	rand.Seed(time.Now().UnixNano()) // Initialize the global pseudo-random generator
-	fmt.Println(utility.RandomFromInterval(1.0, 2.0))
-}
+	func main() {
+		fmt.Println(utility.RandomFromInterval(1.0, 2.0))
+	}
--- utility/utility.go
+++ utility/utility.go
@@ -6,6 +6,6 @@ import (
 
 
 // randomFromInterval generates a random float64 between min and max, inclusive
-func randomFromInterval(min, max float64) float64 {
+func RandomFromInterval(min, max float64) float64 {
     return min + rand.Float64()*(max-min)
 }
